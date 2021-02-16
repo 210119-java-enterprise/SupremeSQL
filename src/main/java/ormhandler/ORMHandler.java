0d6bc556826433da4bebc552691c1cf08cadfb8d
Calvin Zheng <calvin.zheng@revature.net>
2021-02-16 11:01:11 -0600

    Deleted Meta related Files

--- src/main/java/ormhandler/ORMHandler.java
+++ src/main/java/ormhandler/ORMHandler.java
@@ -2,9 +2,6 @@ package ormhandler;
 
 public interface ORMHandler {
 
-    <T> T findbyID(Class<T> clazz, Object key);
-
-    <T> T create(T object);
 
 
 }
--- src/main/java/ormhandler/ORMHandlerConcrete.java
+++ src/main/java/ormhandler/ORMHandlerConcrete.java
@@ -1,8 +1,5 @@
 package ormhandler;
 
-import meta.MetaTable;
-import metahandler.MetaHandler;
-import metahandler.MetaHandlerConcrete;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -15,43 +12,5 @@ import java.util.List;
 
 public class ORMHandlerConcrete implements ORMHandler{
 
-    private MetaHandler metaHandler = new MetaHandlerConcrete(this);
-    private DataSource DS;
 
-    public ORMHandlerConcrete(DataSource DS){
-        this.DS = DS;
-    }
-    @Override
-    public <T> T findbyID(Class<T> clazz, Object key) {
-
-        MetaTable table = metaHandler.getMetaTable(clazz);
-        String sql = "SELECT * FROM " + table.getTableName() + " WHERE "
-                        + table.getIdRow().getOne() + " = ?;";
-        List<T> result = findALL(clazz, Collections.singletonList(key),sql);
-        return result.get(0);
-    }
-
-    private <T> List<T> findALL(Class<T> clazz, List<Object> p,String sql) {
-        List<T> result = new ArrayList<>();
-        try (Connection conn = DS.getConnection();
-             PreparedStatement PS = conn.prepareStatement(sql)) {
-            metaHandler.LoadPStatement(PS, p);
-            try (ResultSet RS = PS.executeQuery()) {
-                while (RS.next()) {
-                    result.add(metaHandler.ResultToObject(clazz, RS));
-                }
-            }
-
-        } catch (SQLException e) {
-            e.getStackTrace();
-        }
-
-        return result;
-
-    }
-
-    @Override
-    public <T> T create(T object) {
-        return null;
-    }
 }
